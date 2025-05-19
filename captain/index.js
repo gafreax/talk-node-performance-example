5e48d89388b98110b9f5a43aa75619c9d7047843
Gabriele Fontana <gafreax@gmail.com>
2025-05-19 16:10:54 +0200

    add better interrogation

--- captain/index.js
+++ captain/index.js
@@ -1,8 +1,6 @@
 import { stdout } from 'process'
 import autocannon from 'autocannon'
 
-[ 'Pirate', 'Voodoo Lady', 'Monkey Island', 'Guybrush Threepwood', 'LeChuck' ].forEach( console.log )
-
 const pirate = {
   name: 'The Voodoo Lady',
   image: 'placeholder_voodoolady.png',
@@ -11,7 +9,11 @@ const pirate = {
 }
 
 const randomPirateName = () => {
-  return `Pirate ${Math.floor(Math.random() * 1000)}`
+  const randomError = Math.floor( Math.random * 2 ) % 2
+  if (randomError) {
+    return 666
+  }
+  return `Pirate ${Math.floor(Math.random() * 10000)}`
 }
 
 const postTest = (url, path, amount, connections = 10) => {
@@ -63,54 +65,6 @@ const postTest = (url, path, amount, connections = 10) => {
 const url = 'http://localhost:6666'
 const paths = [ '/pirate', '/pirate-without-schema']
 paths.forEach(path => {
-  postTest(url, path, 1000, 10)
+  postTest(url, path, 10000, 100)
 })
-
-const getTest = (url, path, amount, connections = 10) => {
-  const i = autocannon({
-    url,
-    amount,
-    connections,
-    bailout: 1,
-    skipAggregateResults: true,
-    requests: [{
-      method: 'GET',
-      path,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      verifyBody: body => {
-        const parsed = JSON.parse(body)
-        if (parsed.length === 0) {
-          return false
-        }
-        if ( typeof parsed[0].name !== 'string' && parsed[0].name.length === 0) {
-          return false
-        }
-      },
-      onResponse (status, body) {
-        if (status !== 200) {
-          console.error(`Error: ${status} ${body}`)
-        }
-      }
-    }]
-  }, (err, result) => {
-    if (err || result.non2xx > 0) {
-      console.error('Error during benchmark:', err || ' Non-2xx responses')
-    } else {
-      console.log(`Benchmark of ${path} completed successfully`)
-      stdout.write(autocannon.printResult(result))
-    }
-  })
-
-  autocannon.track(i, {
-    renderResultsTable: false
-  })
-
-  process.once('SIGINT', () => {
-    i.stop()
-    console.log('Benchmark stopped')
-  })
-}
-
 
